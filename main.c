d82943ac3bbc20e6bac32e55d5a34d650d012529
Mina Sawers <mina.sawers.11@gmail.com>
2025-02-01 08:30:48 +0200

    T1,T2,T3

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 
 int	main()
 {
-    printf("hello");
+    int count;
+
+    //Test the code for task 1,2
+    count = _printf("Hello %s\n","Ahmed");
+    printf("%d\n",count);
+    count = _printf("Your grade is %c\n",'B');
+    printf("%d\n",count);
+    count = _printf("%d %i\n",10,10);
+    printf("%d\n",count);
+
+
+
     return 0;
 }
--- main.h
+++ main.h
@@ -7,4 +7,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int print_char(int c);
+int print_digit(long n, int base);
+int print_str(char *s);
+int print_format(char specifier, va_list ap);
+int _printf(const char *format, ...);
+
 #endif
